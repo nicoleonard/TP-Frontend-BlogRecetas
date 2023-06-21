7b7fedba54a302063bc4713a4b9607bcf00c343e
nicoleonard <nikolasleonard@gmail.com>
2023-06-21 01:24:26 -0300

    agregados componentes de administracion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Inicio } from "./components/views/Inicio"
+import { Admin } from "./components/views/Admin"
+import { Detalles } from "./components/views/Detalles"
+
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/admin" element={<Admin></Admin>}></Route>
+        <Route exact path="/detalles" element={<Detalles></Detalles>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
