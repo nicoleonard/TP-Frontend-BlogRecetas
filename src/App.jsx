f368380dac91ba7e485d0996504c03d1d98cbc57
nicoleonard <nikolasleonard@gmail.com>
2023-06-21 14:58:14 -0300

    agregada ruta de error404

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Inicio } from "./components/views/Inicio"
 import { Admin } from "./components/views/Admin"
 import { Detalles } from "./components/views/Detalles"
+import { Error404 } from "./components/views/Error404"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" element={<Inicio></Inicio>}></Route>
         <Route exact path="/admin" element={<Admin></Admin>}></Route>
         <Route exact path="/detalles" element={<Detalles></Detalles>}></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
       </Routes>
     </BrowserRouter>
   )
