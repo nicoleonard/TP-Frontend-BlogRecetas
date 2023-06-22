ac20474267491e3e622e7f63c26b39cf1339508c
nicoleonard <nikolasleonard@gmail.com>
2023-06-22 19:08:16 -0300

    agregada funcionalidad y advertencia al boton de borrar receta en admin

--- src/components/views/recetas/AdminReceta.jsx
+++ src/components/views/recetas/AdminReceta.jsx
@@ -1,8 +1,28 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { borrarReceta } from "../../helpers/queries";
 
 const AdminReceta = ({ receta }) => {
-  
+  const eliminarReceta = () =>{
+    Swal.fire({
+        title: 'Esta a punto de borrar una receta, esta seguro?',
+        text: "No hay vuelta atras!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#117700',
+        cancelButtonColor: '#ff2255',
+        confirmButtonText: 'Si, borrar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            borrarReceta(receta.id)
+          Swal.fire(
+            'Se ha ido!',
+            'La receta se ha borrado',
+            'success'
+          )
+        }
+      })
+  }
   return (
     <tr>
       <td>{receta.id}</td>
@@ -11,7 +31,7 @@ const AdminReceta = ({ receta }) => {
       <td>{receta.instrucciones}</td>
       <td>
         <Link className="btn btn-warning" to={'/admin/editar-receta/'+receta.id}>Editar</Link>
-        <Button variant="danger">
+        <Button variant="danger" onClick={eliminarReceta}>
           Borrar
         </Button>
       </td>
