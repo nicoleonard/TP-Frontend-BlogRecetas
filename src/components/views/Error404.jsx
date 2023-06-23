be9e609c9fdce2f12a35f64b797475ec3ebed65b
nicoleonard <nikolasleonard@gmail.com>
2023-06-22 22:20:59 -0300

    agregada estructura del componente Error404 (me habia olvidado)

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,7 +1,16 @@
+import { Container,Image } from "react-bootstrap";
 
 const Error404 = () => {
     return(
         <>
+        <Container className="text-center pt-5">
+            
+            <h1>Pagina no encontrada</h1>
+            <h2 className="text-secondary">Error404</h2>
+            <h3 className="text-secondary opacity-50">oops...</h3>
+            
+
+        </Container>
         </>
     );
 };
